8dc386acc9ec071c5a23999f2fc8286e7f1379c1
emberogl <embernebulan@gmail.com>
2023-09-07 13:27:37 +0200

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿document.querySelectorAll('.point').forEach(point => {
+    point.addEventListener('mouseover', () => {
+        document.querySelector(`#${point.getAttribute('data-content')}`).style.display = 'block';
+    });
+    point.addEventListener('mouseout', () => {
+        document.querySelector(`#${point.getAttribute('data-content')}`).style.display = 'none';
+    });
+});
+
+fetch('/output.txt')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('text-content').innerText = data;
+    })
+    .catch(error => console.log("Error:", error));
